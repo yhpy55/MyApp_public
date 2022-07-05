4b1ffea95405442a63414fa8e58eaf6a53da6c72
yhpy55 <105979636+yhpy55@users.noreply.github.com>
2022-07-05 11:10:29 +0900

    hello1 DjangoAdminログイン機能

--- mysite/hello1/views.py
+++ mysite/hello1/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/admin/login/')
 def index(request):
     return HttpResponse("Hello Django. (hello1)")
