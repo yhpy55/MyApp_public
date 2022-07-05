20b221ab8e1c64cac6f6825bb6a671fe3d91b176
yhpy55 <105979636+yhpy55@users.noreply.github.com>
2022-07-05 10:39:07 +0900

    dev ログイン画面認証処理 テスト稼働

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 import hello.views as hello
 import sauna.views as sauna
+import dev.views as dev
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('dev.urls')),
     path('hello/', hello.index),
     path('sauna/', sauna.index),
+    # path('dev/', dev.index),
 ]
