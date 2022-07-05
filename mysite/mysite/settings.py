b7b70a9804c2e28745c0176bc62cda43243b086a
yhpy55 <105979636+yhpy55@users.noreply.github.com>
2022-07-05 15:17:38 +0900

    sauna/index画面修正 金額を3桁カンマ区切りに

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'dev',
     'hello',
     'hello1',
@@ -122,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#####
+NUMBER_GROUPING = 3
