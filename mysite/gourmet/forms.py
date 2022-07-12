9da3698831c486f2aa4ecba65befa3292ba00de6
yhpy55 <105979636+yhpy55@users.noreply.github.com>
2022-07-12 15:29:53 +0900

    gourmet forms.pyの文言変更

--- mysite/gourmet/forms.py
+++ mysite/gourmet/forms.py
@@ -3,4 +3,4 @@ from .models import Gourmet
 
 
 class FindForm(forms.Form):
-    find = forms.CharField(label='検索条件を指定してください。（店舗名、駅名、ジャンル、定休日、禁煙）', required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    find = forms.CharField(label='検索条件(店舗名 or 駅名 or ジャンル or 定休日 or 禁煙)', required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
