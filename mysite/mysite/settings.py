646ad18bb955004312b69811f7222d520dc93754
yhpy55 <105979636+yhpy55@users.noreply.github.com>
2022-07-14 10:39:04 +0900

    staticフォルダ変更

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/Static/'
 STATICFILES_DIRS = [
 STATIC_DIR,
 ]
