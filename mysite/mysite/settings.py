bfbb5cea0326f561d7d15093e01c1519694c8cb9
yhpy55 <105979636+yhpy55@users.noreply.github.com>
2022-07-14 10:28:56 +0900

    logintest style.css 画面変更

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -15,6 +15,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# tepmpatesフォルダへの絶対パスを定義
+TEMPLATE_DIR = os.path.join(BASE_DIR,"Template")
+
+# staticフォルダへの絶対パスを定義
+STATIC_DIR = os.path.join(BASE_DIR,"static")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -65,7 +71,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,6 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+STATIC_DIR,
+]
 
 #####
 NUMBER_GROUPING = 3
